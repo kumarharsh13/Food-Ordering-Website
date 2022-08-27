e83251bd22e91045701dc44bb6c86a6745cdc6de
Harsh Kumar <bigbangkumarharsh13@gmail.com>
2022-08-27 17:16:01 +0530

    Version 1.0

---
+++ README.md
@@ -0,0 +1,51 @@
+# Food Ordering Website
+As the name says it all, it a simple full stack project which is used for ordering food from your favourite restaurant. It is a user friendly and responsive website. 
+
+## Features :
+- The project has two interface i.e. an user interface and an admin interface.
+- At user-end he/she can ony order food and set the quantity as per there requirment.
+- At user-end he/she can also view there profile details and order history.
+- At user-end he/she can change their basic contact details and as well as there password. 
+- At admins-end he/she can perform two task : 
+    - Add new food to the food menu.
+    - View and dispatch order of the customers.
+- Admin can also verify the working of the website from his own end.
+- Use cookies for login into the account.
+
+## Build With :
+<ul>
+    <li><a href="https://www.w3schools.com/html/" target="_blank">HTML</a></li>
+    <li><a href="https://www.w3schools.com/css/" target="_blank">CSS</a></li>
+    <li><a href="https://www.w3schools.com/js/" target="_blank">JavaScript</a></li>
+    <li><a href="https://www.w3schools.com/bootstrap5/index.php" target="_blank">Bootstrap</a></li>
+    <li><a href="https://expressjs.com/" target="_blank">ExpressJS</a></li>
+    <li><a href="https://www.w3schools.com/mysql/default.asp" target="_blank">MySQL</a></li>
+</ul>
+
+## Install and Run :
+- Clone the repository or download the zip folder.
+- Extract the zip folder.
+- Import the data from the ``` FoodOrderingWebsite.sql ``` file to your ``` MySQL``` Database.
+- Install NodeJS in your system.
+- Go to the folder where you have extracted or cloned the project.
+- Open command prompt or terminal having the same location where your project is.
+- Type ``` npm install ``` in your command prompt or terminal.
+- Now to run the project on your server type ``` npm start ``` .
+
+## Screenshot :
+Here are some of the screenshot of Food Ordering Website.
+
+1. Landing Page :<br><br> <img src="screenshot/Landing Page.png"><br><br>
+2. SignUp  Page :<br><br> <img src="screenshot/Sign Up Page.png"><br><br>
+3. SignIn Page :<br><br> <img src="screenshot/Sign In Page.png"><br><br>
+4. Home Page : <br><br> <img src="screenshot/HomePage.png"><br><br>
+5. Menu List : <br><br> <img src="screenshot/Menu Cart.png"><br><br>
+6. Cart Page : <br><br> <img src="screenshot/My Cart Page.png"><br><br>
+7. Confirmation Page :<br><br> <img src="screenshot/Order Confirmation Page.png"><br><br>
+8. Details and Order History Page :<br><br> <img src="screenshot/Details and Order History Page.png"><br><br>
+9. Settings Page :<br><br> <img src="screenshot/Settings Page.png"><br><br>
+10. Sign Out Option :<br><br> <img src="screenshot/Sign Out.png"><br><br>
+11. Admin Sign In Page : <br><br> <img src="screenshot/Admin Login Page.png"><br><br>
+12. Admin Welcome Page :<br><br> <img src="screenshot/Admin Welcome Page.png"><br><br>
+13. Admin Adding Food Page :<br><br> <img src="screenshot/Admin Adding Food Page.png"><br><br>
+14. Admin Order View & Dispatching Page :<br><br> <img src="screenshot/Admin Viewing and Dispatching Order Page.png"><br><br>
---
+++ app.js
@@ -0,0 +1,418 @@
+// Loading and Using Modules Required
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const ejs = require('ejs');
+const fileUpload = require('express-fileupload');
+const { v4: uuidv4 } = require('uuid');
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+app.use(fileUpload());
+
+// Connectiong to Database
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'ROOT_2',
+  password: '1@HelloMoto',
+  database: 'foodorderingwesitedb'
+});
+
+connection.connect();
+
+/*****************************  User-End Portal ***************************/
+
+// Rendering Index Page
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+// Rendering SignUp Page
+app.get("/signup", (req, res) => {
+  res.render("signup");
+});
+
+app.post("/signup", (req, res) => {
+  const name = req.body.name;
+  const address = req.body.address;
+  const email = req.body.email;
+  const mobile = req.body.mobile;
+  const password = req.body.password;
+
+  connection.query('INSERT INTO users (user_name, user_address, user_email, user_password, user_mobileno) VALUES (?, ?, ?, ?, ?)', [name, address, email, password, mobile], function (error, results, fields) {
+    if (error) console.log(error);
+    else res.render('signin');
+  });
+});
+
+// Rendering SignIn Page
+app.get("/signin", (req, res) => {
+  res.render("signin");
+});
+
+app.post("/signin", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  connection.query('SELECT user_email, user_password FROM users WHERE user_email = ?', [email], function (error, results) {
+    if (error) {
+      //res.send("<h1> Invalid Email</h1>");
+      res.render('signin');
+    }
+    else {
+      if (results[0].user_password === password) {
+        connection.query('SELECT user_id, user_name FROM users WHERE user_email = ?', [email], function (error, results) {
+          if (error) res.status(404);
+          else {
+            const userid = results[0].user_id;
+            const username = results[0].user_name;
+            res.cookie('cookuid', userid);
+            res.cookie('cookuname', username);
+            res.redirect('/homepage');
+          }
+        });
+      }
+      else {
+        //res.send("<h1> Invalid Password</h1>");
+        res.render('signin');
+      }
+    }
+  });
+});
+
+// Rendering Home Page
+app.get("/homepage", (req,res) => {
+  const sid = req.cookies.cookuid;
+  const suname = req.cookies.cookuname;
+  connection.query('SELECT user_id, user_name FROM users WHERE user_id = ? and user_name = ?', [sid,suname], function (error, results) {
+    if(!error && results) {
+      connection.query('SELECT * FROM menu',  function (error, results) {
+        if(!error) {
+          res.render('homepage',{username: suname, userid: sid, items: results});
+        }
+      });
+    }
+    else {
+      res.render('signin');
+    }
+  });
+});
+
+// My Cart Detail Accessing
+let citems = [];
+let citemdetails = [];
+let item_in_cart = 0;
+function getItemDetails(citems,size) {
+  citems.map((item) => {
+    connection.query('SELECT * FROM menu WHERE item_id= ?', [item], function(error, results_item) {
+      citemdetails.push(results_item[0]);
+    });
+  });
+  item_in_cart = size;
+}
+
+// Rendering My Cart
+app.get("/cart", (req,res) => {
+  const sid = req.cookies.cookuid;
+  const suname = req.cookies.cookuname;
+  connection.query('SELECT user_id, user_name FROM users WHERE user_id = ? and user_name = ?', [sid,suname], function (error, results) {
+    if(!error && results) {
+      res.render('cart',{username: suname, userid: sid, items: citemdetails, item_count: item_in_cart});
+    }
+    else {
+      res.render('signin');
+    }
+  });
+});
+
+let cart_items = [];
+app.post("/cart", (req,res) => {
+  cart_items = req.body.cart;
+  let unique = [];
+  cart_items.forEach(index => {
+    if(!unique.includes(index)) {
+      unique.push(index);
+    }
+  });
+  getItemDetails(unique,unique.length);
+});
+
+
+// Rendering for checking out
+app.post("/checkout", (req,res) => {
+  const sid = req.cookies.cookuid;
+  const suname = req.cookies.cookuname;
+  connection.query('SELECT user_id, user_name FROM users WHERE user_id = ? and user_name = ?', [sid,suname], function (error, results) {
+    if(!error && results) {
+      connection.query('SELECT * FROM menu',  function (error, results) {
+        if(!error) {
+          const item_id = req.body.itemid;
+          const quantity = req.body.quantity;
+          const total_sub_price = req.body.subprice;
+          const userid = req.cookies.cookuid;
+          let currDate = new Date();
+          if(item_id.length == 1) {
+            if(quantity[0]!=0) {
+              connection.query('INSERT INTO orders (order_id, user_id, item_id, quantity, price, datetime) VALUES (?, ?, ?, ?, ?, ?)',[uuidv4(), userid, item_id, quantity, total_sub_price*quantity, currDate], function(error,results,fields) {
+                if(error) {
+                  console.log(error);
+                  res.send(500);
+                }
+                else {
+                  citems = [];
+                  citemdetails = [];
+                  item_in_cart = 0;
+                  getItemDetails(citems,0);
+                  res.render('confirmation',{username: suname, userid: sid});
+                }
+              });
+            }
+          }
+          else {
+          item_id.map((item,i) => {
+            if(quantity[i]!=0) {
+              connection.query('INSERT INTO orders (order_id, user_id, item_id, quantity, price, datetime) VALUES (?, ?, ?, ?, ?, ?)',[uuidv4(), userid, item, quantity[i], total_sub_price[i]*quantity[i], currDate], function(error,results,fields) {
+                if(error) {
+                  console.log(error);
+                  res.send(500);
+                }
+              });
+            }
+          });
+        }
+        citems = [];
+        citemdetails = [];
+        item_in_cart = 0;
+        getItemDetails(citems,0);
+        res.render('confirmation',{username: suname, userid: sid});
+      }
+      });
+    }
+    else {
+      res.render('signin');
+    }
+  });
+});
+
+
+// Rendering Order Confirmation Page
+app.get("/confirmation",(req,res) => {
+    const sid = req.cookies.cookuid;
+    const suname = req.cookies.cookuname;
+    connection.query('SELECT user_id, user_name FROM users WHERE user_id = ? and user_name = ?', [sid,suname], function (error, results) {
+      if(!error && results) {
+        res.render('confirmation',{username: suname, userid: sid});
+      }
+      else {
+        res.render('signin');
+      }
+    });
+});
+
+app.post("/confirmation", (req,res) => {
+  res.render('confirmation');
+});
+
+// Rendering MyOrder Page
+app.get("/myorders", (req,res) => {
+  const sid = req.cookies.cookuid;
+  const suname = req.cookies.cookuname;
+  connection.query('SELECT user_id, user_name, user_address, user_email, user_mobileno FROM users WHERE user_id = ? and user_name = ?', [sid,suname], function (error, resultUser) {
+    if(!error && resultUser) {
+      connection.query('SELECT order_dispatch.order_id, order_dispatch.user_id, order_dispatch.quantity, order_dispatch.price, order_dispatch.datetime, menu.item_id, menu.item_name, menu.item_img FROM order_dispatch, menu WHERE order_dispatch.user_id = ? AND menu.item_id=order_dispatch.item_id ORDER BY order_dispatch.datetime DESC',[sid],function (error, results) {
+        if(!error) {
+          res.render('myorders',{userDetails: resultUser,  items: results, item_count: item_in_cart});
+        }
+      });
+    }
+    else {
+      res.render('signin');
+    }
+  });
+});
+
+
+// Rendering Settings Page
+app.get("/settings",(req,res) => {
+  const sid = req.cookies.cookuid;
+  const suname = req.cookies.cookuname;
+  connection.query('SELECT user_id, user_name FROM users WHERE user_id = ? and user_name = ?', [sid,suname], function (error, results) {
+    if(!error && results) {
+      res.render('settings',{username: suname, userid: sid, item_count: item_in_cart});
+    }
+  });
+});
+/***************************************** Admin End Portal ********************************************/
+// Rendering Admin SignIn
+app.get("/admin_signin",(req,res) => {
+  res.render('admin_signin');
+});
+
+app.post("/admin_signin", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  connection.query('SELECT admin_email, admin_password FROM admin WHERE admin_email = ?', [email], function (error, results) {
+    if (error) {
+      //res.send("<h1> Invalid Email</h1>");
+      res.render('/signin');
+    }
+    else {
+      if (results[0].admin_password === password) {
+        connection.query('SELECT admin_id, admin_name FROM admin WHERE admin_email = ?', [email], function (error, results) {
+          if (error) res.status(404);
+          else {
+            aid = results[0].admin_id;
+            aname = results[0].admin_name;
+            res.cookie('cookuid', aid);
+            res.cookie('cookuname', aname);
+            res.render('adminHomepage');
+          }
+        });
+      }
+      else {
+        //res.send("<h1> Invalid Password</h1>");
+        res.render("/admin_signin");
+      }
+    }
+  });
+});
+
+// Rendering AdminHomepage
+app.get("/adminHomepage",(req,res) => {
+  const sid = req.cookies.cookuid;
+  const suname = req.cookies.cookuname;
+  connection.query('SELECT admin_id, admin_name FROM admin WHERE admin_email = ? and admin_name = ?', [sid,suname], function (error, results) {
+    if(!error && results) {
+      res.render('adminHomepage',{username: suname, userid: sid, items: results});
+    }
+    else {
+      res.render('admin_signin');
+    }
+  });
+});
+
+// Rendering Admin addmin new food
+app.get("/admin_addFood",(req,res) => {
+  const sid = req.cookies.cookuid;
+  const suname = req.cookies.cookuname;
+  connection.query('SELECT admin_id, admin_name FROM admin WHERE admin_id = ? and admin_name = ?', [sid,suname], function (error, results) {
+    if(!error && results) {
+      res.render('admin_addFood',{username: suname, userid: sid, items: results});
+    }
+    else {
+      res.render('admin_signin');
+    }
+  });
+});
+
+app.post("/admin_addFood",(req,res) => {
+  const fname = req.body.FoodName;
+  const ftype = req.body.FoodType;
+  const fcategory = req.body.FoodCategory;
+  const fserving = req.body.FoodServing;
+  const fcalories = req.body.FoodCalories;
+  const fprice = req.body.FoodPrice;
+  const frating = req.body.FoodRating
+  if(!req.files) {
+    return res.status(400).send("Image was not Uploaded");
+  }
+  const fimage = req.files.FoodImg;
+  const fimage_name = fimage.name;
+  if(fimage.mimetype == "image/jpeg" || fimage.mimetype == "image/png") {
+    fimage.mv('public/images/dish/'+fimage_name, function(err) {
+      if(err) {
+        return res.status(500).send(err);
+      }
+      connection.query('INSERT INTO menu (item_name, item_type, item_category, item_serving, item_calories, item_price, item_rating, item_img) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [fname, ftype, fcategory, fserving, fcalories, fprice, frating, fimage_name], function (error, results, fields) {
+        if (error) console.log(error);
+        else {
+          //res.send("<h1>Item Added Successfully</h1>");
+          res.redirect('/admin_addFood');
+        }
+      });
+    });
+  }
+  else {
+    //res.send("<h1>Not Successfull</h1>")
+    res.render('admin_addFood');
+  }
+});
+
+// Rendering Admin View and Dispatching Orders
+app.get("/admin_view_dispatch_orders",(req,res) => {
+  const sid = req.cookies.cookuid;
+  const suname = req.cookies.cookuname;
+  connection.query('SELECT admin_id, admin_name FROM admin WHERE admin_id = ? and admin_name = ?', [sid,suname], function (error, results) {
+    if(!error && results) {
+      connection.query('SELECT * FROM orders ORDER BY datetime', function (error,results2) {
+        res.render('admin_view_dispatch_orders',{username: suname, userid: sid, orders: results2});
+      });
+    }
+    else {
+      res.render('admin_signin');
+    }
+  });
+});
+
+let totalOrder = [];
+
+app.post("/admin_view_dispatch_orders", (req,res) => {
+  totalOrder = req.body.order_id_s;
+  //console.log(totalOrder);
+  let unique = [];
+  totalOrder.forEach(index => {
+    if(!unique.includes(index)) {
+      unique.push(index);
+    }
+  });
+  //console.log(unique);
+  for(let i=0;i<unique.length;i++) {
+    connection.query('SELECT * FROM orders WHERE order_id = ?',[unique[i]], function(error,resultsItem) {
+      //console.log(resultsItem[0]);
+      if(!error) {
+        let currDate = new Date();
+        connection.query('INSERT INTO order_dispatch (order_id, user_id, item_id, quantity, price, datetime) VALUES (?, ?, ?, ?, ?, ?)',[resultsItem[0].order_id,resultsItem[0].user_id,resultsItem[0].item_id,resultsItem[0].quantity,resultsItem[0].price,currDate], function(error,results) {
+          if(!error) {
+            connection.query('DELETE FROM orders WHERE order_id= ?',[resultsItem[0].order_id], function(error,results2) {
+              if(!error) {
+              }
+              else {
+                res.status(500).send('Something Went Wrong');
+              }
+            });
+          }
+          else {
+            res.status(500).send('Something Went Wrong');
+          }
+        });
+      }
+      else {
+        res.status(500).send('Something Went Wrong');
+      }
+    });
+  }
+
+  connection.query('SELECT * FROM orders ORDER BY datetime', function (error,results2_dis) {
+    res.render('admin_view_dispatch_orders',{username: req.cookies.cookuname, orders: results2_dis});
+  });
+
+
+});
+
+// Rendering Login-Out
+app.get("/logout",(req,res) => {
+  res.clearCookie();
+  return res.redirect("/signin");
+});
+
+// Port Listener
+app.listen(port, () => {
+  console.log(`Listening at port: ${port}`);
+});
